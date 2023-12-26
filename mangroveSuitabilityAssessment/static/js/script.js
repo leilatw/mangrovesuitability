50944cab7a03771770e9be63f6cad0dbdba486fb
leilatw <134103240+leilatw@users.noreply.github.com>
2023-12-26 19:58:30 +0100

    Delete mangroveSuitabilityAssessment directory

--- mangroveSuitabilityAssessment/static/js/script.js
+++
@@ -1,4 +0,0 @@
-// Example JavaScript (optional for this functionality)
-document.getElementById('assessmentForm').addEventListener('submit', function(event) {
-    // You can add form validation or other interactive features here
-});
